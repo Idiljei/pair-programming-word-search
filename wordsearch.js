109a08497f30b7d3e988dfe2ef5e700e3f4c14ee
Idiljei <i.jeilani@gmail.com>
2021-01-14 02:21:23 +0000

    Word Search Joiner

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
+//Coded with : Zoey, Idil, Gabe and Jacob LIGHTHOUSE LABS 
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    // check for rows
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
+    // go through 2d array column by column and build a new array 
+    let newArray = [];
+    for (let i = 0; i < letters[0].length; i++) { 
+        let joinedColumn = [];
+        for (let j = 0; j < letters.length; j++) {
+            joinedColumn.push(letters[j][i]);
+        }
+        newArray.push(joinedColumn);
+    }
+    // check for columns
+    const columnJoin = newArray.map(ls => ls.join(''));
+    for (l of columnJoin) {
+        if (l.includes(word)) return true;
+    }
+ 
+    return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
